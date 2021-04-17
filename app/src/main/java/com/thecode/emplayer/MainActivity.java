7c0b611ff63fd4fe73d21a93d2c471bc2a431b4c
thecode00 <thecode0087@gmail.com>
2021-04-18 03:04:23 +0900

    All New

--- app/src/main/java/com/thecode/emplayer/MainActivity.java
+++ app/src/main/java/com/thecode/emplayer/MainActivity.java
@@ -15,11 +15,15 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -27,8 +31,8 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
-import com.thecode.emplayer.adapter.RecyclerViewAdapter;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -36,11 +40,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity{
 
-    @BindView(R.id.songlist) RecyclerView songListView;
+    @BindView(R.id.songlist) ListView songListView;
 
-    private ArrayList<Song> songList;
-    private RecyclerViewAdapter mAdapter;
-
+    String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,70 +50,95 @@ public class MainActivity extends AppCompatActivity{
 
         ButterKnife.bind(this);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-        songList = new ArrayList<Song>();
-
-        //권한확인용
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permissionCheck == PackageManager.PERMISSION_GRANTED){
-            Log.e("권한확익", "권한있음");
-        }
-        //노래들을 찾는다.
-        findSongList();
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        songListView.setLayoutManager(layoutManager);
-        //TODO 사용방법 알아보기
-        //songListView.setHasFixedSize(true);
-
-        mAdapter = new RecyclerViewAdapter(songList, this);
-        songListView.setAdapter(mAdapter);
+        runtimePermission();
 
     }
 
-    //노래를 찾을때 사용
-    private void findSongList(){
-        ContentResolver contentResolver = getContentResolver();
+    //권한요청
+    public void runtimePermission(){
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        //권한을 얻었을때
+                        displaySongs();
+                    }
 
-        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
-        //아스키코드순으로 정렬
-        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        //권한을 못얻었을때
 
-        String[] projection = {
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.ALBUM_ID,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.ARTIST,
-                MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.DATA
-        };
+                    }
 
-        Cursor cursor = contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, sortOrder);
-        cursor.moveToFirst();
-        Log.d("노래갯수", Integer.toString(cursor.getCount()));
-
-
-
-        //각 노래의 정보를 Song객체에 담아 리스트에 저장한다.
-        //while이 먼저 안오고 do가 먼저오는 이유는 cursor의 갯수가 1개일때 cursor.moveToNext가 false가 되기때문
-        //TODO albumart 부분 해결하기
-        if (cursor != null && cursor.getCount() > 0){
-            do {
-                long song_id = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
-                long album_id = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
-                String song_title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
-                String song_album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
-                String song_artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
-                long mDuration = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
-                String dataPath = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
-                Log.e("123",song_title);
-                songList.add(new Song(song_id, album_id, song_title, song_album, song_artist, mDuration, dataPath));
-
-            } while (cursor.moveToNext());
-        }
-
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
 
+    //노래찾는 메소드
+    public ArrayList<File> findSong(File file){
+        ArrayList<File> arrayList = new ArrayList<>();
 
+        File[] files = file.listFiles();
+
+        for (File singleFile: files){
+            if (singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.addAll(findSong(singleFile));
+            } else {
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")){
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void displaySongs(){
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+        for (int i = 0; i < mySongs.size(); i++){
+            //노래파일 뒤에 있는 mp3와 wav확장명은 보기 안좋으므로 없앤다.
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "")
+                    .replace(".wav", "");
+        }
+
+        //ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        //songListView.setAdapter(myAdapter);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        songListView.setAdapter(customAdapter);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View myView = getLayoutInflater().inflate(R.layout.list_item, null, false);
+            //TODO setSelected뭔기 찾아보기
+            TextView tvSongName = (TextView) myView.findViewById(R.id.songname);
+            tvSongName.setSelected(true);
+            tvSongName.setText(items[position]);
+
+            return myView;
+
+        }
+    }
 }
--- app/src/main/java/com/thecode/emplayer/SongContract.java
+++
@@ -1,5 +0,0 @@
-package com.thecode.emplayer;
-
-public class SongContract {
-    public final static String SONGNAME_TAG = "songname";
-}
